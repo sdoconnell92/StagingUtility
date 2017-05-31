3a878401d1db7223f7a59414fe19c86967ba42e3
Sean O'Connell <sdoconnell92@gmail.com>
2017-05-31 00:36:19 -0500

    cpmfised

--- PyInstaller_Files/PyInstaller_Run_Script.bat
+++ PyInstaller_Files/PyInstaller_Run_Script.bat
@@ -1,2 +1,4 @@
 #!/bin/bash
 pyinstaller -F -w --distpath StagingUtility\PyInstaller_Files\dist --buildpath StagingUtility\PyInstaller_Files\build guiapp.py
+
+pyinstaller -F -w --distpath C:\Users\sean\WorthSaving\repos\StagingUtility\PyInstaller_Files\dist\StagingUtility\PyInstaller_Files\dist --buildpath C:\Users\sean\WorthSaving\repos\StagingUtility\PyInstaller_Files\build\StagingUtility\PyInstaller_Files\build C:\Users\sean\WorthSaving\repos\StagingUtility\nsp_staging_utility\guiapp.py
